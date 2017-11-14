0954699d7172974f315fbafedd7d56f0165f9f13
swilliams19 <32960946+swilliams19@users.noreply.github.com>
2017-11-14 16:37:29 -0600

    S.Williams
    I do not know if I am doing this correctly or at least on the correct path.  The "Tricky" assignment I did not understand the order of the messages. In the StarFigures I can not get past the first step.

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,33 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Stephanie Williams
+  	Date: 11/14/17
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
-
+//5star - *****//
+//2star -  * *//
+//1star -   *//
 
 public class StarFigures {
 
 	public static void main(String[] args) {
-
+		fivestar();
+		twostar();
+		onestar();
+		
 	}
-
+		
+	public static void fivestar() {
+		System.out.println("*****");
+	}
+	
+	public static void twostar() {
+		System.out.println(" * * ");
+	}
+	
+	public static void onestar() {
+		System.out.println("  *  ");
+	}
+	
 }
--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,34 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Stephanie Williams
+  	Date: 11/14/2017
 */
 
 // 1. Your predicted output 
- 
+ 	/*"This is message1."*/
+ 	/*"This is message2."*/
+ 		
 
 public class Tricky {
-
+ 		public static void main(String[]args) {
+ 		message1();
+ 		message2();
+ 		System.out.println("Done with main.");
+ 		
+}
+ 		public static void message1() {
+ 			System.out.println("This is message1.");
+}
+ 		public static void message2() {
+ 			System.out.println("This is message2.");
+ 			message1();
+ 			System.out.println("Done with message 2.");
 }
 
+ 	}
 // 3. Were you correct? Explain any differences
- 
+       /*No, I was not correct.  I only predicted 2 lines of output when there are 5.*/
+	   /*So I failed to predict the last 3 lines of output.*/
+	   /*And even if I had predicted the other lines, I would not have put them in that order.*/
  
 
